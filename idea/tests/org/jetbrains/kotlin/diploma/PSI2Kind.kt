f12b256574e605c8d197ceeb7cc5df94c69f4b3d
tihonovcore <tihonovcore@gmail.com>
2021-03-07 11:47:50 +0300

    [diploma] implement strategies for sparse data

--- idea/tests/org/jetbrains/kotlin/diploma/PSI2Kind.kt
+++ idea/tests/org/jetbrains/kotlin/diploma/PSI2Kind.kt
@@ -8,29 +8,29 @@ package org.jetbrains.kotlin.diploma
 import org.jetbrains.kotlin.psi.*
 
 // TODO: probably PSI contains some more information
-// TODO: `*.text` and `*.name` should be embedded, marked for doing this later or removed
 val psi2kind = object : KtVisitor<String, Void?>() {
+    val strategy: SparseDataStrategy = Drop
 
     override fun visitKtElement(element: KtElement, data: Void?) = element.node.elementType.toString()
 
     override fun visitNamedDeclaration(declaration: KtNamedDeclaration, data: Void?): String {
-        return super.visitNamedDeclaration(declaration, data) + " " + declaration.name
+        return super.visitNamedDeclaration(declaration, data) + strategy.process(" " + declaration.name)
     }
 
     override fun visitConstantExpression(expression: KtConstantExpression, data: Void?): String {
-        return super.visitConstantExpression(expression, data) + " " + expression.text
+        return super.visitConstantExpression(expression, data) + strategy.process(" " + expression.text)
     }
 
     override fun visitSimpleNameExpression(expression: KtSimpleNameExpression, data: Void?): String {
-        return super.visitSimpleNameExpression(expression, data) + " " + expression.getReferencedName()
+        return super.visitSimpleNameExpression(expression, data) + strategy.process(" " + expression.getReferencedName())
     }
 
     override fun visitLiteralStringTemplateEntry(entry: KtLiteralStringTemplateEntry, data: Void?): String {
-        return super.visitLiteralStringTemplateEntry(entry, data) + " " + "\"${entry.text}\""
+        return super.visitLiteralStringTemplateEntry(entry, data) + strategy.process(" \"${entry.text}\"")
     }
 
     override fun visitEscapeStringTemplateEntry(entry: KtEscapeStringTemplateEntry, data: Void?): String {
-        return super.visitEscapeStringTemplateEntry(entry, data) + " " + "\"${entry.text}\""
+        return super.visitEscapeStringTemplateEntry(entry, data) + strategy.process(" \"${entry.text}\"")
     }
 
     // NOTE: there are some more StringEntry
@@ -38,4 +38,24 @@ val psi2kind = object : KtVisitor<String, Void?>() {
     // > visitStringTemplateEntryWithExpression
     // > visitBlockStringTemplateEntry
     // > visitSimpleNameStringTemplateEntry
+}
+
+private sealed class SparseDataStrategy {
+    abstract fun process(data: String): String
+}
+
+private object NoProcess : SparseDataStrategy() {
+    override fun process(data: String): String = data
+}
+
+private object Embeds : SparseDataStrategy() {
+    override fun process(data: String): String = TODO("implement embedding")
+}
+
+private class MarkWith(val marker: String) : SparseDataStrategy() {
+    override fun process(data: String): String = "$marker$data"
+}
+
+private object Drop : SparseDataStrategy() {
+    override fun process(data: String): String = ""
 }
